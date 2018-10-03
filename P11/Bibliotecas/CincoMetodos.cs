3709f982a8166be8205152cf1d27cdebcc49ef24
Carlos <halomau@hotmail.com>
2018-10-03 17:59:30 -0500

    más cambioss

---
+++ P11/Bibliotecas/CincoMetodos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotecas
+{
+    class CincoMetodos
+    {
+        //combinado, solo hielo, shot, personalizado
+        public string Combinado(int alcohol, int refresco, int agua, byte hielo)
+        {
+            int total = alcohol+refresco+agua;
+            if(total>250)
+            {
+                return "Parece que tu trago es demasiado grande para el vaso.";
+            }
+            else if(total <6)
+            {
+                return "Tu trago es demasiado pequeño.";
+            }
+            else()
+        }
+
+    }
+}
